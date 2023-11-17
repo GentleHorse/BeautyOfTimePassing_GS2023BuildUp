00dff9a87b9a2d31c6fb5dd73d8719988ba1331c
GentleHorse <anch.abc.t@gmail.com>
2023-11-17 19:10:03 +0100

    Clean up code, remove GUI

--- src/script.js
+++ src/script.js
@@ -7,19 +7,10 @@ import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
 import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry";
 import { gsap } from "gsap";
-import Stats from "stats.js";
 import glassBrickLightVertexShader from "./shaders/glassBricks/vertex.glsl";
 import glassBrickLightFragmentShader from "./shaders/glassBricks/fragment.glsl";
 import projectorLightVertex from "./shaders/projector/vertex.glsl";
 import projectorLightFragment from "./shaders/projector/fragment.glsl";
-
-
-/**
- * Stats =======================================================
- */
-const stats = new Stats();
-stats.showPanel(0);
-document.body.appendChild(stats.dom);
 
 /**
  * Loaders ============================================================
@@ -70,8 +61,8 @@ const textureLoader = new THREE.TextureLoader();
  * Base =================================================================
  */
 // Debug
-const gui = new dat.GUI({ width: 400 });
-gui.close();
+// const gui = new dat.GUI({ width: 400 });
+// gui.close();
 
 // Debug
 const debugObject = {};
@@ -81,14 +72,6 @@ const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
-
-// AxesHelper
-const axesHelper = new THREE.AxesHelper(3);
-axesHelper.visible = false;
-const helperFolder = gui.addFolder("Helper");
-helperFolder.close();
-helperFolder.add(axesHelper, "visible");
-scene.add(axesHelper);
 
 /**
  * Overlay ==============================================================
@@ -127,7 +110,6 @@ const updateAllMaterials = () => {
       child instanceof THREE.Mesh &&
       child.material instanceof THREE.MeshStandardMaterial
     ) {
-      // child.material.envMap = environmentMap
       child.material.envMapIntensity = debugObject.envMapIntensity;
       child.material.needsUpdate = true;
       child.castShadow = true;
@@ -170,7 +152,9 @@ const hdrEquirect = new RGBELoader().load(
 );
 
 // normal texture for glass bricks
-const organicNormalTexture = textureLoader.load('/textures/glassNormal/organic_02.jpg');
+const organicNormalTexture = textureLoader.load(
+  "/textures/glassNormal/organic_02.jpg"
+);
 
 /**
  * 3D Text ====================================================================
@@ -197,14 +181,6 @@ fontLoader.load("/fonts/Vollkorn_Regular.json", (font) => {
   const text = new THREE.Mesh(textGeometry, textMaterial);
   text.rotation.set(0, Math.PI, 0);
   text.position.set(2.1, 1.6, 0.08);
-  const textFolder = gui.addFolder("3D Text");
-  textFolder.close();
-  textFolder.add(text.position, "x", -10, 10, 0.001).name("PosX");
-  textFolder.add(text.position, "y", -10, 10, 0.001).name("PosY");
-  textFolder.add(text.position, "z", -10, 10, 0.001).name("PosZ");
-  textFolder.add(text.rotation, "x", -Math.PI, Math.PI, 0.001).name("RotateX");
-  textFolder.add(text.rotation, "y", -Math.PI, Math.PI, 0.001).name("RotateY");
-  textFolder.add(text.rotation, "z", -Math.PI, Math.PI, 0.001).name("RotateZ");
   scene.add(text);
 });
 
@@ -378,87 +354,6 @@ laptopScreen_01.rotation.set(
 );
 scene.add(laptopScreen_01);
 
-// GUI
-const laptopScreenFolder_00 = gui.addFolder("laptop_00");
-laptopScreenFolder_00.close();
-laptopScreenFolder_00
-  .add(laptopScreenParams.screen_00, "scale", 0, 1, 0.001)
-  .onChange(() => {
-    laptopScreen_00.scale.x = laptopScreenParams.screen_00.scale;
-    laptopScreen_00.scale.y = laptopScreenParams.screen_00.scale;
-    laptopScreen_00.scale.z = laptopScreenParams.screen_00.scale;
-  });
-laptopScreenFolder_00
-  .add(laptopScreenParams.screen_00, "position_x", -10, 10, 0.0001)
-  .onChange(() => {
-    laptopScreen_00.position.x = laptopScreenParams.screen_00.position_x;
-  });
-laptopScreenFolder_00
-  .add(laptopScreenParams.screen_00, "position_y", -10, 10, 0.0001)
-  .onChange(() => {
-    laptopScreen_00.position.y = laptopScreenParams.screen_00.position_y;
-  });
-laptopScreenFolder_00
-  .add(laptopScreenParams.screen_00, "position_z", -10, 10, 0.0001)
-  .onChange(() => {
-    laptopScreen_00.position.z = laptopScreenParams.screen_00.position_z;
-  });
-laptopScreenFolder_00
-  .add(laptopScreenParams.screen_00, "rotation_x", -10, 10, 0.001)
-  .onChange(() => {
-    laptopScreen_00.rotation.x = laptopScreenParams.screen_00.rotation_x;
-  });
-laptopScreenFolder_00
-  .add(laptopScreenParams.screen_00, "rotation_y", -10, 10, 0.001)
-  .onChange(() => {
-    laptopScreen_00.rotation.y = laptopScreenParams.screen_00.rotation_y;
-  });
-laptopScreenFolder_00
-  .add(laptopScreenParams.screen_00, "rotation_z", -10, 10, 0.001)
-  .onChange(() => {
-    laptopScreen_00.rotation.z = laptopScreenParams.screen_00.rotation_z;
-  });
-
-const laptopScreenFolder_01 = gui.addFolder("laptop_01");
-laptopScreenFolder_01.close();
-laptopScreenFolder_01
-  .add(laptopScreenParams.screen_01, "scale", 0, 1, 0.001)
-  .onChange(() => {
-    laptopScreen_01.scale.x = laptopScreenParams.screen_01.scale;
-    laptopScreen_01.scale.y = laptopScreenParams.screen_01.scale;
-    laptopScreen_01.scale.z = laptopScreenParams.screen_01.scale;
-  });
-laptopScreenFolder_01
-  .add(laptopScreenParams.screen_01, "position_x", -10, 10, 0.001)
-  .onChange(() => {
-    laptopScreen_01.position.x = laptopScreenParams.screen_01.position_x;
-  });
-laptopScreenFolder_01
-  .add(laptopScreenParams.screen_01, "position_y", -10, 10, 0.001)
-  .onChange(() => {
-    laptopScreen_01.position.y = laptopScreenParams.screen_01.position_y;
-  });
-laptopScreenFolder_01
-  .add(laptopScreenParams.screen_01, "position_z", -10, 10, 0.001)
-  .onChange(() => {
-    laptopScreen_01.position.z = laptopScreenParams.screen_01.position_z;
-  });
-laptopScreenFolder_01
-  .add(laptopScreenParams.screen_01, "rotation_x", -10, 10, 0.001)
-  .onChange(() => {
-    laptopScreen_01.rotation.x = laptopScreenParams.screen_01.rotation_x;
-  });
-laptopScreenFolder_01
-  .add(laptopScreenParams.screen_01, "rotation_y", -10, 10, 0.001)
-  .onChange(() => {
-    laptopScreen_01.rotation.y = laptopScreenParams.screen_01.rotation_y;
-  });
-laptopScreenFolder_01
-  .add(laptopScreenParams.screen_01, "rotation_z", -10, 10, 0.001)
-  .onChange(() => {
-    laptopScreen_01.rotation.z = laptopScreenParams.screen_01.rotation_z;
-  });
-
 /**
  * Glass Bricks ==========================================================
  */
@@ -474,17 +369,14 @@ const glassBrickGeometry = new THREE.BoxGeometry(
   glassBrickParams.size.width,
   glassBrickParams.size.length,
   glassBrickParams.size.depth
-)
+);
 
 // group 00
 let glassBrickGroupPosition_00 = 0;
 let glassBricksMeshGroup_00 = new THREE.Group();
 scene.add(glassBricksMeshGroup_00);
 for (let i = 0; i < 10; i++) {
-  const glassBrickMesh = new THREE.Mesh(
-    glassBrickGeometry,
-    glassBrickMaterial
-  );
+  const glassBrickMesh = new THREE.Mesh(glassBrickGeometry, glassBrickMaterial);
   glassBrickMesh.scale.set(
     glassBrickParams.scale,
     glassBrickParams.scale,
@@ -493,35 +385,18 @@ for (let i = 0; i < 10; i++) {
   glassBrickMesh.position.set(0, glassBrickGroupPosition_00, 0);
   glassBricksMeshGroup_00.add(glassBrickMesh);
 
-  glassBrickGroupPosition_00 += glassBrickParams.size.length * 1.03 * glassBrickParams.scale;
+  glassBrickGroupPosition_00 +=
+    glassBrickParams.size.length * 1.03 * glassBrickParams.scale;
 }
 
 glassBricksMeshGroup_00.position.set(1.5, -0.9, 0);
-
-const glassBricksFolder_00 = gui.addFolder("glassBricks_00");
-glassBricksFolder_00.close();
-glassBricksFolder_00
-  .add(glassBricksMeshGroup_00.position, "x", -10, 10, 0.001)
-  .name("posX");
-glassBricksFolder_00
-  .add(glassBricksMeshGroup_00.position, "y", -10, 10, 0.001)
-  .name("posY");
-glassBricksFolder_00
-  .add(glassBricksMeshGroup_00.position, "z", -10, 10, 0.001)
-  .name("posZ");
-glassBricksFolder_00
-  .add(glassBricksMeshGroup_00.rotation, "y", 0, Math.PI, 0.001)
-  .name("rotate");
 
 // group 01
 let glassBrickGroupPosition_01 = 0;
 let glassBricksMeshGroup_01 = new THREE.Group();
 scene.add(glassBricksMeshGroup_01);
 for (let i = 0; i < 10; i++) {
-  const glassBrickMesh = new THREE.Mesh(
-    glassBrickGeometry,
-    glassBrickMaterial
-  );
+  const glassBrickMesh = new THREE.Mesh(glassBrickGeometry, glassBrickMaterial);
   glassBrickMesh.scale.set(
     glassBrickParams.scale,
     glassBrickParams.scale,
@@ -530,27 +405,12 @@ for (let i = 0; i < 10; i++) {
   glassBrickMesh.position.set(0, glassBrickGroupPosition_01, 0);
   glassBricksMeshGroup_01.add(glassBrickMesh);
 
-  glassBrickGroupPosition_01 += glassBrickParams.size.length * 1.03 * glassBrickParams.scale;
+  glassBrickGroupPosition_01 +=
+    glassBrickParams.size.length * 1.03 * glassBrickParams.scale;
 }
 
 glassBricksMeshGroup_01.position.set(0.597, -0.9, -0.654);
 glassBricksMeshGroup_01.rotation.y = 2.736;
-
-const glassBricksFolder_01 = gui.addFolder("glassBricks_01");
-glassBricksFolder_01.close();
-glassBricksFolder_01
-  .add(glassBricksMeshGroup_01.position, "x", -10, 10, 0.001)
-  .name("posX");
-glassBricksFolder_01
-  .add(glassBricksMeshGroup_01.position, "y", -10, 10, 0.001)
-  .name("posY");
-glassBricksFolder_01
-  .add(glassBricksMeshGroup_01.position, "z", -10, 10, 0.001)
-  .name("posZ");
-glassBricksFolder_01
-  .add(glassBricksMeshGroup_01.rotation, "y", 0, Math.PI, 0.001)
-  .name("rotate");
-
 
 /**
  * Points of interest ==========================================================
@@ -565,62 +425,6 @@ const pointsOfInterestLocationsParams = {
   pointsOfInterest_03: { x: -1.49, y: 0.056, z: -2.01 },
   pointsOfInterest_04: { x: -1.36, y: -0.316, z: -2.403 },
 };
-
-// test meshes
-// const testMesh_00 = new THREE.Mesh(
-//   new THREE.SphereGeometry(0.03, 10, 10),
-//   new THREE.MeshNormalMaterial()
-// );
-// testMesh_00.position.set(
-//   pointsOfInterestLocationsParams.pointsOfInterest_00.x,
-//   pointsOfInterestLocationsParams.pointsOfInterest_00.y,
-//   pointsOfInterestLocationsParams.pointsOfInterest_00.z
-// );
-// scene.add(testMesh_00);
-
-// const testMesh_01 = new THREE.Mesh(
-//   new THREE.SphereGeometry(0.03, 10, 10),
-//   new THREE.MeshNormalMaterial()
-// );
-// testMesh_01.position.set(
-//   pointsOfInterestLocationsParams.pointsOfInterest_01.x,
-//   pointsOfInterestLocationsParams.pointsOfInterest_01.y,
-//   pointsOfInterestLocationsParams.pointsOfInterest_01.z
-// );
-// scene.add(testMesh_01);
-
-// const testMesh_02 = new THREE.Mesh(
-//   new THREE.SphereGeometry(0.03, 10, 10),
-//   new THREE.MeshNormalMaterial()
-// );
-// testMesh_02.position.set(
-//   pointsOfInterestLocationsParams.pointsOfInterest_02.x,
-//   pointsOfInterestLocationsParams.pointsOfInterest_02.y,
-//   pointsOfInterestLocationsParams.pointsOfInterest_02.z
-// );
-// scene.add(testMesh_02);
-
-// const testMesh_03 = new THREE.Mesh(
-//   new THREE.SphereGeometry(0.03, 10, 10),
-//   new THREE.MeshNormalMaterial()
-// );
-// testMesh_03.position.set(
-//   pointsOfInterestLocationsParams.pointsOfInterest_03.x,
-//   pointsOfInterestLocationsParams.pointsOfInterest_03.y,
-//   pointsOfInterestLocationsParams.pointsOfInterest_03.z
-// );
-// scene.add(testMesh_03);
-
-// const testMesh_04 = new THREE.Mesh(
-//   new THREE.SphereGeometry(0.03, 10, 10),
-//   new THREE.MeshNormalMaterial()
-// );
-// testMesh_04.position.set(
-//   pointsOfInterestLocationsParams.pointsOfInterest_04.x,
-//   pointsOfInterestLocationsParams.pointsOfInterest_04.y,
-//   pointsOfInterestLocationsParams.pointsOfInterest_04.z
-// );
-// scene.add(testMesh_04);
 
 // points of interests
 const offset = 0.1;
@@ -667,112 +471,6 @@ const points = [
   },
 ];
 
-// GUI
-const pointsOfInterestLocationFolder_00 = gui.addFolder("pointsOfInterest_00");
-pointsOfInterestLocationFolder_00.close();
-pointsOfInterestLocationFolder_00
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_00, "x", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_00.position.x =
-      pointsOfInterestLocationsParams.pointsOfInterest_00.x;
-  });
-pointsOfInterestLocationFolder_00
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_00, "y", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_00.position.y =
-      pointsOfInterestLocationsParams.pointsOfInterest_00.y;
-  });
-pointsOfInterestLocationFolder_00
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_00, "z", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_00.position.z =
-      pointsOfInterestLocationsParams.pointsOfInterest_00.z;
-  });
-
-const pointsOfInterestLocationFolder_01 = gui.addFolder("pointsOfInterest_01");
-pointsOfInterestLocationFolder_01.close();
-pointsOfInterestLocationFolder_01
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_01, "x", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_01.position.x =
-      pointsOfInterestLocationsParams.pointsOfInterest_01.x;
-  });
-pointsOfInterestLocationFolder_01
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_01, "y", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_01.position.y =
-      pointsOfInterestLocationsParams.pointsOfInterest_01.y;
-  });
-pointsOfInterestLocationFolder_01
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_01, "z", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_01.position.z =
-      pointsOfInterestLocationsParams.pointsOfInterest_01.z;
-  });
-
-const pointsOfInterestLocationFolder_02 = gui.addFolder("pointsOfInterest_02");
-pointsOfInterestLocationFolder_02.close();
-pointsOfInterestLocationFolder_02
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_02, "x", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_02.position.x =
-      pointsOfInterestLocationsParams.pointsOfInterest_02.x;
-  });
-pointsOfInterestLocationFolder_02
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_02, "y", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_02.position.y =
-      pointsOfInterestLocationsParams.pointsOfInterest_02.y;
-  });
-pointsOfInterestLocationFolder_02
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_02, "z", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_02.position.z =
-      pointsOfInterestLocationsParams.pointsOfInterest_02.z;
-  });
-
-const pointsOfInterestLocationFolder_03 = gui.addFolder("pointsOfInterest_03");
-pointsOfInterestLocationFolder_03.close();
-pointsOfInterestLocationFolder_03
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_03, "x", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_03.position.x =
-      pointsOfInterestLocationsParams.pointsOfInterest_03.x;
-  });
-pointsOfInterestLocationFolder_03
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_03, "y", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_03.position.y =
-      pointsOfInterestLocationsParams.pointsOfInterest_03.y;
-  });
-pointsOfInterestLocationFolder_03
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_03, "z", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_03.position.z =
-      pointsOfInterestLocationsParams.pointsOfInterest_03.z;
-  });
-
-const pointsOfInterestLocationFolder_04 = gui.addFolder("pointsOfInterest_04");
-pointsOfInterestLocationFolder_04.close();
-pointsOfInterestLocationFolder_04
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_04, "x", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_04.position.x =
-      pointsOfInterestLocationsParams.pointsOfInterest_04.x;
-  });
-pointsOfInterestLocationFolder_04
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_04, "y", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_04.position.y =
-      pointsOfInterestLocationsParams.pointsOfInterest_04.y;
-  });
-pointsOfInterestLocationFolder_04
-  .add(pointsOfInterestLocationsParams.pointsOfInterest_04, "z", -10, 10, 0.001)
-  .onChange(() => {
-    testMesh_04.position.z =
-      pointsOfInterestLocationsParams.pointsOfInterest_04.z;
-  });
-
 /**
  * Sizes =======================================================================
  */
@@ -805,7 +503,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.set(3.595, 1.118, -1.75);
+camera.position.set(3.986, 0.335, -2.272);
 scene.add(camera);
 
 // Controls
@@ -813,11 +511,11 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 // GUI
-const cameraFolder = gui.addFolder("camera");
-cameraFolder.close();
-cameraFolder.add(camera.position, "x", -10, 10, 0.001);
-cameraFolder.add(camera.position, "y", -10, 10, 0.001);
-cameraFolder.add(camera.position, "z", -10, 10, 0.001);
+// const cameraFolder = gui.addFolder("camera");
+// cameraFolder.close();
+// cameraFolder.add(camera.position, "x", -10, 10, 0.001);
+// cameraFolder.add(camera.position, "y", -10, 10, 0.001);
+// cameraFolder.add(camera.position, "z", -10, 10, 0.001);
 
 /**
  * Renderer ====================================================================
@@ -840,8 +538,6 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 const clock = new THREE.Clock();
 
 const tick = () => {
-  stats.begin();
-
   const elapsedTime = clock.getElapsedTime();
 
   // Update controls
@@ -884,8 +580,6 @@ const tick = () => {
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick);
-
-  stats.end();
 };
 
 tick();
